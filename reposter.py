6077dd0e5986185b9ffb7a050c90f86671091df5
Nytrock <savvazhirnov@gmail.com>
2025-01-02 18:49:30 +0300

    Add reactions, better tracking, structure refactoring

--- reposter.py
+++ reposter.py
@@ -1,9 +1,10 @@
+import asyncio
 import os
 from io import BytesIO
-from time import sleep
 
 import urllib.request
 import requests
+from pyrogram.types import Message
 
 import vk_parser
 import pyrogram
@@ -12,24 +13,18 @@ import json
 import re
 
 
-def send_all_text():
-    mes = app.send_message(channel, texts[0], disable_web_page_preview=False)
-    if len(texts) > 0:
-        for txt in texts[1:]:
-            mes = app.send_message(channel, txt, disable_web_page_preview=False,
-                                   reply_to_message_id=mes.id)
-    return mes
-
-
-with open('config.json') as file:
-    config = json.load(file)
+with open('config.json') as config_file:
+    config = json.load(config_file)
 
 default_sleep_time = 5
 error_sleep_time = 30
+max_media_text_len = 1024
+max_text_len = 4096
+emoji = '🔥'
 
 channel = '@' + config['CHANNEL_NAME']
-vk_login = config["VK_LOGIN"]
-vk_password = config["VK_PASSWORD"]
+vk_login = config['VK_LOGIN']
+vk_password = config['VK_PASSWORD']
 
 app_id = config['TELEGRAM_APP_API_ID']
 app_hash = config['TELEGRAM_APP_API_HASH']
@@ -37,123 +32,139 @@ user_name = config['USER_NAME']
 
 app = pyrogram.Client(user_name, app_id, app_hash)
 app.start()
-data = vk_parser.get_last_post()
-while data is None:
-    sleep(default_sleep_time)
+
+
+async def send_all_text(texts: list[str]) -> Message:
+    mes = await app.send_message(channel, texts[0], disable_web_page_preview=False)
+    if len(texts) > 0:
+        for txt in texts[1:]:
+            mes = await app.send_message(channel, txt, disable_web_page_preview=False,
+                                   reply_to_message_id=mes.id)
+    await app.send_reaction(channel, mes.id, emoji)
+    return mes
+
+
+async def main():
     data = vk_parser.get_last_post()
-
-last_post = data['text']
-
-while True:
-    try:
+    while data is None:
+        await asyncio.sleep(default_sleep_time)
         data = vk_parser.get_last_post()
-        while data is None:
-            sleep(default_sleep_time)
+    last_post = data
+
+    while True:
+        try:
             data = vk_parser.get_last_post()
+            while data is None:
+                await asyncio.sleep(default_sleep_time)
+                data = vk_parser.get_last_post()
 
-        if data['text'] != last_post:
-            last_post = data['text']
-            media = []
-            documents = []
-            files = []
-            isOtherMedia = False
-            last_message = None
+            if data['id'] != last_post['id']:
+                last_post = data
+                media = []
+                documents = []
+                files = []
+                is_other_media = False
+                last_message = None
 
-            text = re.sub('([#].+?)@nytrock', r'\1', data['text']).replace("#nytrock ", "")
-            matches = re.findall(r"[[].+?[|].+?[]]", text)
-            if matches:
-                for match in matches:
-                    link_parts = match[1:-1].split('|')
-                    link = f"<a href='https://vk.com/{link_parts[0]}'>{link_parts[1]}</a>"
-                    text = text.replace(match, link)
-            texts = [text]
-            if len(text) > 4096:
-                texts = []
-                start = 0
-                end = 4096
-                for i in range(len(text) // 4096 + 2):
-                    if i != len(text) // 4096 + 1:
-                        end = text[:end].rfind(" ")
-                    texts.append(text[start:min(end, len(text))])
-                    start = end + 1
-                    end += 4096
+                text = re.sub('([#].+?)@nytrock', r'\1', data['text']).replace("#nytrock ", "")
+                matches = re.findall(r"[[].+?[|].+?[]]", text)
+                if matches:
+                    for match in matches:
+                        link_parts = match[1:-1].split('|')
+                        link = f"<a href='https://vk.com/{link_parts[0]}'>{link_parts[1]}</a>"
+                        text = text.replace(match, link)
+                texts = [text]
+                if len(text) > max_text_len:
+                    texts = []
+                    start = 0
+                    end = max_text_len
+                    for i in range(len(text) // max_text_len + 2):
+                        if i != len(text) // max_text_len + 1:
+                            end = text[:end].rfind(" ")
+                        texts.append(text[start:min(end, len(text))])
+                        start = end + 1
+                        end += max_text_len
 
-            for attachment in data['attachments']:
-                if attachment['type'] == 'photo':
-                    sizes = {}
-                    for size in attachment['photo']['sizes']:
-                        sizes[size['type']] = size['url']
+                for attachment in data['attachments']:
+                    if attachment['type'] == 'photo':
+                        sizes = {}
+                        for size in attachment['photo']['sizes']:
+                            sizes[size['type']] = size['url']
 
-                    url = str()
-                    if 'w' in sizes.keys():
-                        url = sizes['w']
-                    elif 'z' in sizes.keys():
-                        url = sizes['z']
-                    elif 'y' in sizes.keys():
-                        url = sizes['y']
-                    elif 'x' in sizes.keys():
-                        url = sizes['x']
-                    elif 'm' in sizes.keys():
-                        url = sizes['m']
+                        url = str()
+                        if 'w' in sizes.keys():
+                            url = sizes['w']
+                        elif 'z' in sizes.keys():
+                            url = sizes['z']
+                        elif 'y' in sizes.keys():
+                            url = sizes['y']
+                        elif 'x' in sizes.keys():
+                            url = sizes['x']
+                        elif 'm' in sizes.keys():
+                            url = sizes['m']
 
-                    image = requests.get(url).content
-                    media.append(pyrogram.types.InputMediaPhoto(BytesIO(image)))
-                elif attachment['type'] == 'video':
-                    link = f"https://vk.com/video{attachment['video']['owner_id']}_{attachment['video']['id']}"
-                    file_name = link.split("/")[-1] + ".mp4"
-                    ydl_opts = {"username": vk_login, "password": vk_password, "outtmpl": file_name}
+                        image = requests.get(url).content
+                        media.append(pyrogram.types.InputMediaPhoto(BytesIO(image)))
+                    elif attachment['type'] == 'video':
+                        link = f"https://vk.com/video{attachment['video']['owner_id']}_{attachment['video']['id']}"
+                        file_name = link.split("/")[-1] + ".mp4"
+                        ydl_opts = {"username": vk_login, "password": vk_password, "outtmpl": file_name}
 
-                    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                        ydl.download([link])
+                        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                            ydl.download([link])
 
-                    media.append(pyrogram.types.InputMediaVideo(file_name))
-                    files.append(file_name)
-                elif attachment['type'] == 'doc':
-                    if attachment['doc']['ext'] == 'gif':
-                        if len(text) < 1024:
-                            app.send_animation(channel, attachment['doc']['url'], text)
+                        media.append(pyrogram.types.InputMediaVideo(file_name))
+                        files.append(file_name)
+                    elif attachment['type'] == 'doc':
+                        if attachment['doc']['ext'] == 'gif':
+                            if len(text) < max_media_text_len:
+                                last_message = await app.send_animation(channel, attachment['doc']['url'], text)
+                            else:
+                                message = await send_all_text(texts)
+                                last_message = await app.send_animation(channel, attachment['doc']['url'],
+                                                                  reply_to_message_id=message.id)
+                            is_other_media = True
                         else:
-                            message = send_all_text()
-                            last_message = app.send_animation(channel, attachment['doc']['url'],
-                                                              reply_to_message_id=message.id)
-                        isOtherMedia = True
+                            urllib.request.urlretrieve(attachment['doc']['url'], attachment['doc']['title'])
+                            documents.append(pyrogram.types.InputMediaDocument(attachment['doc']['title']))
+                            files.append(attachment['doc']['title'])
+
+                    elif attachment['type'] == 'poll':
+                        answers = list(map(lambda x: x['text'], attachment['poll']['answers']))
+                        message = await send_all_text(texts)
+                        last_message = await app.send_poll(channel, attachment['poll']['question'], answers,
+                                                     reply_to_message_id=message.id)
+                        is_other_media = True
+
+                if is_other_media:
+                    await app.send_reaction(channel, last_message.id, emoji)
+                    if documents:
+                        await app.send_media_group(channel, documents, reply_to_message_id=last_message.id)
+                    continue
+
+                if not media:
+                    if documents:
+                        documents[0].caption = text
+                        await app.send_media_group(channel, documents)
                     else:
-                        urllib.request.urlretrieve(attachment['doc']['url'], attachment['doc']['title'])
-                        documents.append(pyrogram.types.InputMediaDocument(attachment['doc']['title']))
-                        files.append(attachment['doc']['title'])
-
-                elif attachment['type'] == 'poll':
-                    answers = list(map(lambda x: x['text'], attachment['poll']['answers']))
-                    message = send_all_text()
-                    last_message = app.send_poll(channel, attachment['poll']['question'], answers,
-                                                 reply_to_message_id=message.id)
-                    isOtherMedia = True
-
-            if isOtherMedia:
-                if documents:
-                    app.send_media_group(channel, documents, reply_to_message_id=last_message.id)
-                continue
-
-            if not media:
-                if documents:
-                    documents[0].caption = text
-                    app.send_media_group(channel, documents)
+                        await send_all_text(texts)
                 else:
-                    app.send_message(channel, text)
-            else:
-                if len(text) < 1024:
-                    media[0].caption = text
-                    last_message = app.send_media_group(channel, media)[0]
-                else:
-                    message = send_all_text()
-                    last_message = app.send_media_group(channel, media, reply_to_message_id=message.id)[0]
+                    if len(text) < max_media_text_len:
+                        media[0].caption = text
+                        last_message = (await app.send_media_group(channel, media))[0]
+                    else:
+                        message = await send_all_text(texts)
+                        last_message = (await app.send_media_group(channel, media, reply_to_message_id=message.id))[0]
+                    await app.send_reaction(channel, last_message.id, emoji)
 
-                if documents:
-                    app.send_media_group(channel, documents, reply_to_message_id=last_message.id)
+                    if documents:
+                        await app.send_media_group(channel, documents, reply_to_message_id=last_message.id)
 
-            for file in files:
-                os.remove(file)
+                for file in files:
+                    os.remove(file)
+            await asyncio.sleep(default_sleep_time)
+        except ConnectionResetError:
+            await asyncio.sleep(error_sleep_time)
 
-        sleep(default_sleep_time)
-    except ConnectionResetError:
-        sleep(error_sleep_time)
+
+asyncio.run(main())
